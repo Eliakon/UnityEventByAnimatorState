dbe76565849c4d4891b9842023ba3cebf7509bb8
Estelle Martinez <rikku.shimizu@gmail.com>
2017-01-19 12:06:53 +0100

    Define UnityEvents in scene to be called by OnStateEnter

---
+++ EventByAnimatorStateManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public struct EventByAnimatorState
+{
+    public string StateName;
+    public UnityEvent Action;
+}
+
+public class EventByAnimatorStateManager : MonoBehaviour
+{
+    public EventByAnimatorState[] EventsByState;
+}
---
+++ OnEnterStateBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class OnEnterStateBehaviour : StateMachineBehaviour
+{
+    private EventByAnimatorStateManager eventManager;
+    private bool getComponentDone;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!getComponentDone)
+        {
+            eventManager = animator.GetComponent<EventByAnimatorStateManager>();
+            getComponentDone = true;
+        }
+
+        if (null == eventManager)
+        {
+            Debug.LogWarningFormat("No EventByAnimatorStateManager linked to animator {0}", name);
+            return;
+        }
+
+        var eventByState = Array.Find(eventManager.EventsByState, ebs => stateInfo.IsName(ebs.StateName));
+
+        if (null == eventByState.Action)
+        {
+            return;
+        }
+
+        eventByState.Action.Invoke();
+    }
+}
